b8fd64dd651c012dab8b25a9404a33028192b032
Nathan Lawrence <nathan.m.e.lawrence@gmail.com>
2024-10-16 20:33:28 +0100

    Adding logging

--- src/main/kotlin/uk/co/mutuallyassureddistraction/paketliga/Main.kt
+++ src/main/kotlin/uk/co/mutuallyassureddistraction/paketliga/Main.kt
@@ -9,6 +9,7 @@ import org.jdbi.v3.postgres.PostgresPlugin
 import org.jdbi.v3.sqlobject.SqlObjectPlugin
 import org.jdbi.v3.sqlobject.kotlin.KotlinSqlObjectPlugin
 import org.jdbi.v3.sqlobject.kotlin.onDemand
+import org.slf4j.LoggerFactory
 import uk.co.mutuallyassureddistraction.paketliga.dao.GameDao
 import uk.co.mutuallyassureddistraction.paketliga.dao.GuessDao
 import uk.co.mutuallyassureddistraction.paketliga.dao.PointDao
@@ -17,6 +18,7 @@ import uk.co.mutuallyassureddistraction.paketliga.extensions.*
 import uk.co.mutuallyassureddistraction.paketliga.matching.*
 import java.sql.Connection
 import java.sql.DriverManager
+import kotlin.math.log
 
 val PG_JDBC_URL = env("POSTGRES_JDBC_URL")
 val PG_PASSWORD = env("POSTGRES_PASSWORD")
@@ -25,18 +27,21 @@ val SERVER_ID = Snowflake(
 )
 
 private val BOT_TOKEN = env("BOT_TOKEN") // Get the bot' token from the env vars or a .env file
+private class PKLBOT {}
 
 suspend fun main() {
-
+    val logger = LoggerFactory.getLogger(PKLBOT::class.java)
+    logger.info("Initialising database")
     val connection = DriverManager.getConnection(PG_JDBC_URL, "postgres", PG_PASSWORD)
     if(connection.isValid(0)) {
         val jdbi = getJdbi(connection)
-
+        logger.info("Init DAO's")
         // initialise GameDao and GameExtension
         val gameDao = jdbi.onDemand<GameDao>()
         val guessDao = jdbi.onDemand<GuessDao>()
         val pointDao = jdbi.onDemand<PointDao>()
         val winDao = jdbi.onDemand<WinDao>()
+        logger.info("Init Services")
         val gameFinderService = GameFinderService(gameDao)
         val guessUpsertService = GuessUpsertService(guessDao, gameDao)
         val guessFinderService = GuessFinderService(guessDao)
@@ -53,6 +58,7 @@ suspend fun main() {
         val endGameExtension = EndGameExtension(gameEndService, SERVER_ID)
         val leaderboardExtension = LeaderboardExtension(leaderboardService, SERVER_ID)
 
+        logger.info("Creating bot")
         val bot = ExtensibleBot(BOT_TOKEN) {
             chatCommands {
                 enabled = true
@@ -69,7 +75,7 @@ suspend fun main() {
                 add { leaderboardExtension }
             }
         }
-
+        logger.info("Starting Bot. Beep boop")
         bot.start()
     }
 
