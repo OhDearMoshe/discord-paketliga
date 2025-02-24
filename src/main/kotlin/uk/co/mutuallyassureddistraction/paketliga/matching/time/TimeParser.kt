5d96d16899fd138e4214e327b7122a2fff10508d
OhDearMoshe <nathan.m.e.lawrence@gmail.com>
2025-02-24 19:26:21 +0000

    Coercing to beggining of the day to help with simple inputs

--- src/main/kotlin/uk/co/mutuallyassureddistraction/paketliga/matching/time/TimeParser.kt
+++ src/main/kotlin/uk/co/mutuallyassureddistraction/paketliga/matching/time/TimeParser.kt
@@ -17,7 +17,12 @@ class TimeParser {
     }
 
     fun parseDate(dateString: String): ZonedDateTime {
-        val parsedDate = parser.parse(dateString, Date(), hawkingConfiguration, "eng")
+        // I am not proud of this but it coerces it to be at the start of the day
+        // so that simple times work a bit better
+        val date = Date()
+        date.hours = 2
+        date.minutes = 0
+        val parsedDate = parser.parse(dateString, date, hawkingConfiguration, "eng")
         val dateText = parsedDate.parserOutputs.first().dateRange.startDateFormat
         return fromParsedHawkingString(dateText).withSecond(0)
     }
